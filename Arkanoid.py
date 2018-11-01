20eaa23a7b21c8e89ab3bcf6dd95147efda1cf89
lali1290 <43975840+lali1290@users.noreply.github.com>
2018-11-01 14:53:23 -0500

    Arkanoid.py

---
+++ Arkanoid.py
@@ -0,0 +1,33 @@
+#importando la libreria 
+import pygame
+
+#iniciando el motor de juego
+pygame.init()
+
+# colores definidos por el monitor RGB
+NEGRO  = (0,0,0)#0 significa que no hay color 
+BLANCO = (255,255,255)
+VERDE  = (0,255,0)
+ROJO   = (255,0,0)
+
+#establecer el ancho y alto de la pantalla 
+size=[900,500]
+screen=pygame.display.set_mode(size)#para abrir una ventana 
+
+#crencion de las clases 
+class bola:
+    pygame.draw.circle()#creando la bola
+    
+    
+class Labarra:
+    
+    
+    
+class Barras:
+    
+    
+pygame.display.flip() #mostrar todo lo que se coloca en la pantalla
+def main():
+    print pygame.ver
+if __name__=="__main__":
+    main()
