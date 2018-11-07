8471e62d03084e49aa7b4572a01f2172a7b6db0c
lali1290 <43975840+lali1290@users.noreply.github.com>
2018-11-07 15:11:48 -0500

    Update Arkanoid.py

--- Arkanoid.py
+++ Arkanoid.py
@@ -1,124 +1,138 @@
-import pygame,sys
+import pygame,sys,random
 from pygame.locals import *
 
-pygame.display.set_caption("Proyecto lp")#es para darle nombre a la ventana
-# colores definidos por el monitor RGB
-NEGRO  = (0,0,0)#0 significa que no hay color 
+pygame.display.set_caption("Proyecto lp")
+NEGRO  = (0,0,0)
 BLANCO = (255,255,255)
 VERDE  = (0,255,0)
 ROJO   = (255,0,0)
 VIOLETA = (98, 0, 255)
 
 #crencion de las clases 
-class bola:
+class bola(pygame.sprite.Sprite):
     def __init__(self,screen,color,x,y,width):
-        self.color=ROJO#el color
-        self.posx=x#el centro del circulo
-        self.posy=y#el radio del circulo
-        self.width=width#anchuta de la pelotita
-        self.screen=screen#tiene que ir en una superficie, en este caso la pantalla ezzzz:u
-        self.x2=1#es la velocidad a la que se mueve en el eje x
-        self.y2=1#es la velocidad a la que se mueve en el eje y
+        pygame.sprite.Sprite.__init__(self)
+        self.color=ROJO
+        self.posx=x
+        self.posy=y
+        self.width=width
+        self.screen=screen
+        self.x2=1
+        self.y2=1
         self.rect=Rect(self.posx,self.posy,self.width,self.width)
+
+    def coliPB(self):
+        self.posx += self.x2
+        self.y2 *= -1
     
     #circle(Surface, color, pos, radius,anchura)
     def crea(self,objeto):
-        self.posx += self.x2#hace que se emueva
-        self.posy += self.y2#hace que se mueva
-        self.rect=Rect(self.posx,self.posy,self.width,self.width)#lo tengo que poner porque si no no me deja ejecutar:u
-        pygame.draw.circle(self.screen,self.color,(int(self.posx),int(self.posy)),self.width)#creando la bola
+        self.posx += self.x2
+        self.posy += self.y2
+        #self.screen=(0,0,0)
+        self.rect=Rect(self.posx,self.posy,self.width,self.width)
+        pygame.draw.circle(self.screen,self.color,(int(self.posx),int(self.posy)),self.width)
+        bola.coliPB
+        if self.posx < 10 or self.posx > 890:
+            self.x2 *= -1 
 
-        if self.posx < 10 or self.posx > 890:#si choca con la parte inferior o superior de la pantalla
-            self.x2 *= -1 #cambia de direccion
-
-        if self.posy < 10 or self.posy > 490:#si choca con la parte izquierda o derecha de la pantalla
-            self.y2 *= -1 #cambia de direccion  
-        
-        if self.rect.colliderect(objeto):#para la colision,si colisiona con otro objeto, en este caso la barra que se mueve abajo 
-            self.y2 *= -1#solo tiene que cmbiar la direccion de y, ya ue solo choca con la barra
-
+        if self.posy < 10 or self.posy > 490:
+            self.y2 *=-1
+            
+        if self.rect.colliderect(objeto):
+            self.y2 *= -1
+            
 class Labarra:
     def __init__(self,screen,color,x,y):
         self.color=color#color del rectangulo
-        self.a=100#largo de la barra
-        self.h=10#ancho de la barra
-        self.x=x#posicion en la que esta
-        self.y=y#posicion en la que esta
+        self.a=50
+        self.h=10
+        self.x=x
+        self.y=y
         self.rect = Rect(self.x,self.y,self.a,self.h)#x,y,ancho,altura
         self.screen=screen#pantalla
-        self.x2=0#velocidad de movimiento
+        self.x2=0#velocidad
         
     def crear(self,x2):
         self.rect = Rect(self.x,self.y,self.a,self.h)
-        pygame.draw.rect(self.screen,self.color,self.rect)#dibujando la barra
-        if (self.x>0 and x2==-5) or (self.x<900-self.a and x2==5):#para que no salga de los bordes
-            self.x += x2 #movimiento :u
-
-def main():
-    pygame.init()#iniciando el motor de juego
-    size=(900,500)#establecer el ancho y alto de la pantalla 
-    screen=pygame.display.set_mode(size)#para abrir una ventana 
-    reloj = pygame.time.Clock()#es para no me acuerdo :3
-    obj1=bola(screen,ROJO,450,400,10)#llamando a la bola
-    obj2=Labarra(screen,VIOLETA,450,490)#llamando al rectangulo
-    while True:
-        f=pygame.key.get_pressed()
-        screen.fill(BLANCO)#lleno la pantalla del color branco
-        reloj.tick(150)#es el cambio de bits?bits por segundo?? o algo asi. es como la actualizacion de actualizacion de la pantalla
-        obj1.crea(obj2.rect)#llamo a crear la bola
-        obj2.crear(0)#llamo a crear la barra inferior 
-        pygame.display.flip()#mostrar todo lo que se coloca en la pantalla, lo actualiza
-        for event in pygame.event.get():# Iteramos sobre cada evento de la cola de eventos            
-            if event.type == pygame.QUIT:# Verificamos el tipo de evento.
-                pygame.quit()#si no lo pongo el prograama se cuelga
-                sys.exit()#ni la mas minima idea :u
-        if f[pygame.K_LEFT]:#para que se mueva con la tecla
-            obj2.crear(-5)
-        if f[pygame.K_RIGHT]:#para que se mueva con la tecla
-            obj2.crear(5)
+        pygame.draw.rect(self.screen,self.color,self.rect)
+        if (self.x>0 and x2==-5) or (self.x<900-self.a and x2==5):
+            self.x += x2 
             
-        
-if __name__=="__main__":
-    main()
-    
-    las barras 
-    
-las barras de arriba
 class Barra(pygame.sprite.Sprite):
     def __init__(self,screen,color,x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.barras=pygame.Surface((100,30))
+        self.image=pygame.Surface((100,30))
         self.color=color
-        #self.barras.fill(color)
-        #self.rect=self.barras.get_rect()
+        self.image.fill(color)
         self.x=x
         self.y=y
         self.rect=Rect(self.x,self.y,100,30)
         self.screen=screen#pantalla
-        barras=pygame.draw.rect(self.screen,self.color,self.rect)
-     
-def main():
-    pygame.init()#iniciando el motor de juego
-    size=(900,500)#establecer el ancho y alto de la pantalla 
-    screen=pygame.display.set_mode(size)#para abrir una ventana 
-    screen.fill(BLANCO)
-    reloj = pygame.time.Clock()#es para no me acuerdo :3
-    agrupar=[]
-    for x in range (9):
-        for y in range (3):
-            c1 =random.uniform(1,254)
-            c2 =random.uniform(1,254)
-            c3 =random.uniform(1,254)
-            colores=[c1,c2,c3]
-            agrupar.append(Barra(screen,colores,x*100,y*30))
-
+"""     
+    def colision(self,bola):
+        self.rect=Rect(self.x,self.y,100,30)
+        if self.rect.colliderect(bola):
+                barras.sprite.kill()
+            bola.crear()
+  """
+class Agrupar(Barra):
+    def __init__(self):
+        pass
     
+    def generar(self,barras,screen):
+        for x in range (9):
+            for y in range (3):
+                c1 =random.randint(1,254)
+                c2 =random.randint(1,254)
+                c3 =random.randint(1,254)
+                barra=Barra(screen,(c1,c2,c3),x*100,y*30)
+                barras.add(barra)
+
+def main():
+    pygame.init()
+    size=(900,500)
+    screen=pygame.display.set_mode(size) 
+    screen.fill(BLANCO)
+    
+    reloj = pygame.time.Clock()
+    
+    obj1=bola(screen,ROJO,450,480,10)
+    obj2=Labarra(screen,VIOLETA,450,490)
+    
+    lista=Agrupar()#llamo a las barras de arriba 
+    holis= pygame.sprite.Group()
+    lista.generar(holis,screen)
+    
+    pelota=pygame.sprite.Group()
+    pelota.add(obj1)
+    
+    pygame.display.flip()
     while True:
         f=pygame.key.get_pressed()
-        reloj.tick(150)#es el cambio de bits?bits por segundo?? o algo asi. es como la actualizacion de actualizacion de la pantalla
-        pygame.display.flip()#mostrar todo lo que se coloca en la pantalla, lo actualiza
-        for event in pygame.event.get():# Iteramos sobre cada evento de la cola de eventos            
-            if event.type == pygame.QUIT:# Verificamos el tipo de evento.
-                pygame.quit()#si no lo pongo el prograama se cuelga
-                sys.exit()#ni la mas minima idea :u
-            
+        
+        listaCol = pygame.sprite.groupcollide(holis,pelota,True,False)
+        
+        if len(listaCol)==1:
+            obj1.coliPB()
+        for event in pygame.event.get():            
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        if f[pygame.K_LEFT]:
+            obj2.crear(-5)
+        if f[pygame.K_RIGHT]:
+            obj2.crear(5)
+        if obj1.posy==500:
+            obj1.x2=0
+            obj1.y2=0
+        
+        screen.fill(BLANCO)
+        obj1.crea(obj2.rect)#llamo a crear la bola
+        obj2.crear(0)
+        holis.draw(screen)
+        reloj.tick(150)
+        pygame.display.flip()  
+
+if __name__=="__main__":
+    main()
